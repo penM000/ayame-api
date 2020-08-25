de13ada8b868351cfb07d74b13b41559a9eefd0d
server <server@server1.yukkuriikouze.com>
2020-08-25 18:45:46 +0900

    mongodbの圧縮アルゴリズムをzstdに変更

--- python_script/install_module.sh
+++ python_script/install_module.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-apk add --no-cache build-base mariadb-connector-c-dev libxml2-dev libxslt-dev 
-pip3 install fastapi uvicorn motor aiofiles asyncio bs4 lxml requests
+apk add --no-cache build-base mariadb-connector-c-dev libxml2-dev libxslt-dev snappy-dev
+pip3 install fastapi uvicorn motor aiofiles asyncio bs4 lxml requests python-snappy
